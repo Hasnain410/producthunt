6d625d501e4df1ae86343f07a73a07be267d2fa1
Hasnain Mahmood <hasnain.mahmood1995@gmail.com>
2018-08-25 16:40:17 -0400

    created product details page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Product
@@ -26,11 +26,12 @@ def create(request):
             product.pub_date = timezone.datetime.now()
             product.hunter = request.user
             product.save()
-            return redirect('home')
-
-
-
+            return redirect('/products/' + str(product.id))
         else:
             return render (request, 'products/create.html', {'error': 'All fields are required'})
     else:
         return render (request, 'products/create.html',)
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/detail.html', {'product': product})
